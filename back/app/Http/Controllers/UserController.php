871141dae1546d0b19084be82c93c9d0efddd0e3
Rafael <raortega8906@gmail.com>
2024-02-07 02:02:43 +0100

    Add index method of the api and connect it with the path, modification of the welcome.blade file

--- back/app/Http/Controllers/UserController.php
+++ back/app/Http/Controllers/UserController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+
+    public function index(){
+
+        $users = User::all();
+
+        if($users->count() == 0){
+
+            return response()->json([
+                'message' => 'No hay usuarios registrados.'
+            ],404);
+
+        }
+
+        return response()->json([
+            'data' => $users
+        ]);
+
+    }
+
 }
