216c4a8675eaacc0590018fab435e0d6609dcc25
Rafael <raortega8906@gmail.com>
2024-02-15 23:32:06 +0100

    Update AuthContext

--- front/src/context/AuthContext.jsx
+++ front/src/context/AuthContext.jsx
@@ -1,21 +1,24 @@
-import { createContext, useContext, useState } from "react";
-import axios from "../api/axios";
-import { useNavigate } from "react-router-dom";
+// A espera a implementar
 
-const AuthContext = createContext({});
 
-export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-
-    const csrf = () => {
-        axios.get('/sanctum/csrf-cookie')
-    }
-
-    const getUser = async () => {
-        const {data} = await axios.get('api/user');
-        setUser(data);
-    }
-
-}
+// import { createContext, useContext, useState } from "react";
+// import axios from "../api/axios";
+// import { useNavigate } from "react-router-dom";
+//
+// const AuthContext = createContext({});
+//
+// export const AuthProvider = ({ children }) => {
+//     const [user, setUser] = useState(null);
+//
+//     const csrf = () => {
+//         axios.get('/sanctum/csrf-cookie')
+//     }
+//
+//     const getUser = async () => {
+//         const {data} = await axios.get('api/user');
+//         setUser(data);
+//     }
+//
+// }
 
 
